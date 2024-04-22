fdc4a29f529d619ff589247aff399bad4820c5d1
Gio Formichella <gioformichella@gmail.com>
2024-04-22 18:06:24 +0200

    Setting domain to initialization value if given

--- cell.py
+++ cell.py
@@ -1,4 +1,7 @@
 class Cell:
     def __init__(self, value=None):
         self.value = value
-        self.domain = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        if self.value is not None:
+            self.domain = [self.value]
+        else:
+            self.domain = [1, 2, 3, 4, 5, 6, 7, 8, 9]
