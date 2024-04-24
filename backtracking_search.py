60743f80295e79cb9676b5e1f1d85649090164af
Gio Formichella <gioformichella@gmail.com>
2024-04-24 16:39:55 +0200

    Working on backtracking_search: implemented unassigned variable selection method and value ordering for the variable

---
+++ backtracking_search.py
@@ -0,0 +1,68 @@
+import random
+
+import numpy as np
+
+
+def backtracking_search(board) -> np.ndarray or None:
+    return backtrack(board)
+
+
+def backtrack(board) -> np.ndarray or None:
+    # Checking for complete assignment
+    found = False
+    for i in range(9):
+        for j in range(9):
+            if board[i, j].value is None:
+                found = True
+    if not found:
+        return board
+
+    var = select_unassigned_variable(board)
+    for value in order_domain_values(board, var):
+        # if value is consistent with assignment
+        pass
+
+
+def select_unassigned_variable(csp, strategy="static") -> tuple or None:
+    unassigned_var = []
+    for i in range(9):
+        for j in range(9):
+            if csp[i, j].value is None:
+                unassigned_var.append((i, j))
+
+    match strategy:
+        case "static":  # Choosing first variable in static ordering
+            return unassigned_var[0]
+        case "random":  # Randomly selecting unassigned variable
+            idx = random.randint(0, len(unassigned_var))
+            return unassigned_var[idx]
+        # can add more
+
+
+def order_domain_values(board: np.ndarray, var: tuple) -> list:
+    i = var[0]
+    j = var[1]
+    least_constraining_value = []  # Will store for every value in variable domain (value, constraint_score)
+    neighbors = []  # Stores neighbouring variables
+    # Adding neighbors
+    for k in range(9):
+        if k != j:
+            neighbors.append((i, k))
+        if k != i:
+            neighbors.append((k, j))
+    sr = i // 3  # Square row of variable
+    sc = j // 3  # Square column of variable
+    for m in range(sr * 3, sr * 3 + 3):
+        for n in range(sc * 3, sc * 3 + 3):
+            if (m, n) != (i, j):
+                neighbors.append((m, n))
+
+    for value in board[i, j].domain:
+        count = 0  # Times value is present in neighbors' domain
+        for n in neighbors:
+            if value in board[n[0], n[1]].domain:
+                count += 1
+        least_constraining_value.append((value, count))
+
+    least_constraining_value.sort(key=lambda x: x[1])
+    return least_constraining_value
