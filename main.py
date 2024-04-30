54382f617b097f5cf842769afd429a1bbbc3c7b6
Gio Formichella <gioformichella@gmail.com>
2024-04-30 17:56:11 +0200

    Added print statements for min_conflict_solver

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from backtracking_solver import backtracking_solver
-
+from min_conflicts_solver import min_conflicts_solver
 from sudoku import Sudoku
 
-puzzle = Sudoku(3)
+puzzle = Sudoku(3).difficulty(0.5)
+
+print(puzzle.show())
 
 result, assignments, backtracks = backtracking_solver(puzzle)
 for i in range(9):
@@ -12,3 +14,12 @@ for i in range(9):
 
 print(f"Assignments made: {assignments}")
 print(f"Backtracks made: {backtracks}")
+
+result = min_conflicts_solver(puzzle, max_steps=100000)
+if result is not None:
+    for i in range(9):
+        print(f"{result[i, 0].value}  {result[i, 1].value}  {result[i, 2].value}  {result[i, 3].value}  "
+              f"{result[i, 4].value}  {result[i, 5].value}  {result[i, 6].value}  {result[i, 7].value}  "
+              f"{result[i, 8].value}")
+else:
+    print("No result found")
